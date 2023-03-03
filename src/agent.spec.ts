0ec36ffaf8416bbb7d733c65f668c53b7a0e5aa1
Kayaba Attribution <jdavidgomezca@gmail.com>
2023-03-03 17:47:37 -0500

    Feat: mock multiple NFT transfer

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -89,7 +89,7 @@ const createBatchContractInfo = (
     floorPrice: number,
     from: string,
     to: string,
-    tokenId: string
+    tokenId: string | string[]
 ): [BatchContractInfo[], TransactionEvent] => {
     let mockApiDataArray: BatchContractInfo[] = [];
 
@@ -154,31 +154,58 @@ const createBatchContractInfo = (
                         Web3EthAbi.encodeParameter('address', to),
                     ],
                     "data": SeaPortOrderData,
-                    "logIndex": 71,
+                    "logIndex": 1,
                     "blockNumber": 16217012,
                     "blockHash": "0x4b1f94a7fc5ca5bb74bde07406c1187b0d4dc12c4aacb11972cf2e7fe5fc9608",
-                    "transactionIndex": 39,
-                    "transactionHash": "0x1234",
-                    "removed": false
-                },
-                {
-                    "address": address,
-                    "topics": [
-                        "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-                        Web3EthAbi.encodeParameter('address', from),
-                        Web3EthAbi.encodeParameter('address', to),
-                        Web3EthAbi.encodeParameter('uint256', tokenId),
-                    ],
-                    "data": "0x",
-                    "logIndex": 72,
-                    "blockNumber": 16217012,
-                    "blockHash": "0x4b1f94a7fc5ca5bb74bde07406c1187b0d4dc12c4aacb11972cf2e7fe5fc9608",
-                    "transactionIndex": 40,
+                    "transactionIndex": 1,
                     "transactionHash": "0x1234",
                     "removed": false
                 }
             ]
         } as any);
+
+    if (tokenId instanceof Array) {
+        console.log("tokenId is an array")
+        for (let i = 0; i < tokenId.length; i++) {
+            let newTransferLog = {
+                "address": address,
+                "topics": [
+                    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+                    Web3EthAbi.encodeParameter('address', from),
+                    Web3EthAbi.encodeParameter('address', to),
+                    Web3EthAbi.encodeParameter('uint256', tokenId[i]),
+                ],
+                "data": "0x",
+                "logIndex": i + 1,
+                "blockNumber": 16217012,
+                "blockHash": "0x4b1f94a7fc5ca5bb74bde07406c1187b0d4dc12c4aacb11972cf2e7fe5fc9608",
+                "transactionIndex": 1 + 1,
+                "transactionHash": "0x1234",
+                "removed": false
+            }
+            
+            mockEvent.logs.push(newTransferLog);
+        }
+    } else {
+        mockEvent.logs.push(
+            {
+                "address": address,
+                "topics": [
+                    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+                    Web3EthAbi.encodeParameter('address', from),
+                    Web3EthAbi.encodeParameter('address', to),
+                    Web3EthAbi.encodeParameter('uint256', tokenId),
+                ],
+                "data": "0x",
+                "logIndex": 2,
+                "blockNumber": 16217012,
+                "blockHash": "0x4b1f94a7fc5ca5bb74bde07406c1187b0d4dc12c4aacb11972cf2e7fe5fc9608",
+                "transactionIndex": 2,
+                "transactionHash": "0x1234",
+                "removed": false
+            }
+        );
+    }
 
     mockApiDataArray.push(mockApiData);
 
@@ -220,7 +247,7 @@ describe("high tether transfer agent", () => {
                 10,
                 victim.address,
                 attacker.address,
-                "777"
+                ['1', '2']
             )
             const findings = await handleTransaction(criticalEvent, mockApi);
             console.log(findings);
